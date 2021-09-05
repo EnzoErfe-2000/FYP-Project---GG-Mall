7d17e0c8a14bd6d834ea1bae1ee314492c47a6fa
enzoerfe2000 <enzoerfe2000@gmail.com>
2021-09-05 17:04:06 +0800

    Moved index.php header into header.php Added include_once 'header.php' into index.php

--- index.php
+++ index.php
@@ -1,37 +1,10 @@
 <?php
 require("db/common.php");
+include_once 'header.php';
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="format-detection" content="telephone=no" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, minimal-ui"/>
-    
-    <!-- fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Questrial|Raleway:700,900" rel="stylesheet">
 
-    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="css/bootstrap.extension.css" rel="stylesheet" type="text/css" />
-    <link href="css/style.css" rel="stylesheet" type="text/css" />
-    <link href="css/swiper.css" rel="stylesheet" type="text/css" />
-    <link href="css/sumoselect.css" rel="stylesheet" type="text/css" />
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-
-    <link rel="shortcut icon" href="img/favicon.ico" />
-  	<title>EX ZO</title>
-</head>
-<body>
-
-    <!-- LOADER -->
-    <div id="loader-wrapper"></div>
-
-    <div id="content-block">
-        <!-- HEADER -->
-        <header>
             <div class="header-top">
                 <div class="content-margins">
                     <div class="row">
