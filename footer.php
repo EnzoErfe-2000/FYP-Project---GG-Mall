31417995071b6dd4fbb2ad9e0cf2bd7ef10081d9
enzoerfe2000 <enzoerfe2000@gmail.com>
2021-09-05 17:10:35 +0800

    Added footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+</div>
+</body>
+</html>
+
+<script src="js/jquery-2.2.4.min.js"></script>
+<script src="js/swiper.jquery.min.js"></script>
+<script src="js/global.js"></script>
+
+<!-- styled select -->
+<script src="js/jquery.sumoselect.min.js"></script>
+
+<!-- counter -->
+<script src="js/jquery.classycountdown.js"></script>
+<script src="js/jquery.knob.js"></script>
+<script src="js/jquery.throttle.js"></script>
