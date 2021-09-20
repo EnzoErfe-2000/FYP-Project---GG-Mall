d95381427bd97c3db01f3c149d3a3aeeef296f19
BEH JIA MIN <1191202274@student.mmu.edu.my>
2021-09-20 12:41:47 +0000

    Upload New File

---
+++ products/Logitech_G502_Hero_High_Performance_Gaming_Mouse.php
@@ -0,0 +1,275 @@
+<?php
+include_once '../include/header.php';	
+?>
+
+        <div class="header-empty-space"></div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9 col-md-push-3">
+                    <div class="row">
+                        <div class="col-sm-6 col-xs-b30 col-sm-b0">
+                            
+                            <div class="main-product-slider-wrapper swipers-couple-wrapper">
+                                <div class="swiper-container swiper-control-top">
+                                   <div class="swiper-button-prev hidden"></div>
+                                   <div class="swiper-button-next hidden"></div>
+                                   <div class="swiper-wrapper">
+                                       <div class="swiper-slide">
+                                            <div class="swiper-lazy-preloader"></div>
+                                            <div class="product-big-preview-entry swiper-lazy" data-background="../product_img/LogitechG502_gm.png"></div>
+                                       </div>
+									</div>
+                                </div>
+
+                                <div class="empty-space col-xs-b30 col-sm-b60"></div>
+
+                                <div class="swiper-container swiper-control-bottom" data-breakpoints="1" data-xs-slides="3" data-sm-slides="3" data-md-slides="4" data-lt-slides="4" data-slides-per-view="5" data-center="1" data-click="1">
+                                   <div class="swiper-button-prev hidden"></div>
+                                   <div class="swiper-button-next hidden"></div>
+                                   <div class="swiper-wrapper">
+                                       <div class="swiper-slide">
+                                            <div class="product-small-preview-entry">
+                                                <img src="../product_img/LogitechG502_gm~1.png" alt="" />
+                                            </div>
+                                        </div>
+                                   </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="simple-article size-3 grey col-xs-b5">Gaming Mice</div>
+                            <div class="h3 col-xs-b25"><span id="prod_name">Logitech G502 Hero High Performance Gaming Mouse</div>
+                            <?php echo '<script type="text/javascript">document.title = "GG Mall | " + document.getElementById("prod_name").innerText;</script>';?>
+                            <div class="row col-xs-b25">
+                                <div class="col-sm-6">
+                                    <div class="simple-article size-5 grey">PRICE: <span class="color">RM178.90</span></div>        
+                                </div>
+                                <div class="col-sm-6 col-sm-text-right">
+                                    <div class="rate-wrapper align-inline">
+                                        <i class="fa fa-star" aria-hidden="true"></i>
+                                        <i class="fa fa-star" aria-hidden="true"></i>
+                                        <i class="fa fa-star" aria-hidden="true"></i>
+                                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                                    </div>
+                                    <div class="simple-article size-2 align-inline">0 Reviews</div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="simple-article size-3 col-xs-b5">ITEM NO.: <span class="grey">07</span></div>
+                                </div>
+                                <div class="col-sm-6 col-sm-text-right">
+                                    <div class="simple-article size-3 col-xs-b20">AVAILABLE.: <span class="grey">YES</span></div>
+                                </div>
+                            </div>
+                            <div class="simple-article size-3 col-xs-b30">
+							• HERO 25K DPI<br>
+							• 11 Buttons Fully Programmable<br>
+							• Five 3.6g adjustable weights<br>
+							• Weight – 121g (mouse only)<br>
+							• USB report rate – 1ms
+							</div>
+                            <div class="row col-xs-b40">
+                                <div class="col-sm-3">
+                                    <div class="h6 detail-data-title size-1">quantity:</div>
+                                </div>
+                                <div class="col-sm-9">
+                                    <div class="quantity-select">
+                                        <span class="minus"></span>
+                                        <span class="number">1</span>
+                                        <span class="plus"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row m5 col-xs-b40">
+                                <div class="col-sm-6 col-xs-b10 col-sm-b0">
+                                    <a class="button size-2 style-2 block" href="#">
+                                        <span class="button-wrapper">
+                                            <span class="icon"><img src="img/icon-2.png" alt=""></span>
+                                            <span class="text">add to cart</span>
+                                        </span>
+                                    </a>
+                                </div>
+                                <div class="col-sm-6">
+                                    <a class="button size-2 style-1 block noshadow" href="#">
+                                    <span class="button-wrapper">
+                                        <span class="icon"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+                                        <span class="text">add to favourites</span>
+                                    </span>
+                                </a>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <div class="h6 detail-data-title size-2">share:</div>
+                                </div>
+                                <div class="col-sm-9">
+                                    <div class="follow light">
+                                        <a class="entry" href="#"><i class="fa fa-facebook"></i></a>
+                                        <a class="entry" href="#"><i class="fa fa-twitter"></i></a>
+                                        <a class="entry" href="#"><i class="fa fa-linkedin"></i></a>
+                                        <a class="entry" href="#"><i class="fa fa-google-plus"></i></a>
+                                        <a class="entry" href="#"><i class="fa fa-pinterest-p"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="empty-space col-md-b70"></div>
+
+                </div>
+                <div class="col-md-3 col-md-pull-9">
+                    <div class="h4 col-xs-b10">popular categories</div>
+                    <ul class="categories-menu transparent">
+                        <li>
+                            <a href="#">PC Gaming Peripheral</a>
+                            <div class="toggle"></div>
+                            <ul>
+                                <li>
+                                    <a href="#">Gaming Headset</a>
+                                    <div class="toggle"></div>
+                                    <ul>
+                                        <li>
+                                            <a href="../products/STEELSERIES_ARCTIS_5_7.1_SURROUND_RGB_GAMING_HEADSET.php">SteelSeries Arctis 5 7.1 Surround RGB Gaming Headset</a>
+                                        </li>
+                                        <li>
+                                            <a href="../products/Razer_Blackshark_V2_X_Esports_Gaming_Headset.php">Razer Blackshark V2 X Esports Gaming Headset</a>
+                                        </li>
+                                        <li>
+                                            <a href="../products/Razer_Kraken_Bluetooth_Kitty_Edition_Wireless_Gaming_RGB_Headset.php">Razer Kraken Bluetooth Kitty Edition Wireless Gaming RGB Headset</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">Gaming Keyboard</a>
+                                    <div class="toggle"></div>
+                                    <ul>
+                                        <li>
+                                            <a href="../products/SteelSeries_Apex_3_Water_Resistant_Gaming_Keyboard.php">SteelSeries Apex 3 Water Resistant Gaming Keyboard</a>
+                                        </li>
+                                        <li>
+                                            <a href="../products/Cooler_Master _CK530_V2_TKL_RGB_Gaming_Keyboard.php">Cooler Master CK530 V2 TKL RGB Gaming Keyboard</a>
+                                        </li>
+										<li>
+                                            <a href="../products/Razer_BlackWidow_Green_Mechanical_Gaming_Keyboard.php">Razer BlackWidow Green Mechanical Gaming Keyboard</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">Gaming Mice</a>
+									<div class="toggle"></div>
+                                    <ul>
+                                        <li>
+                                            <a href="../products/Logitech_G502_Hero_High_Performance_Gaming_Mouse.php">Logitech G502 Hero High Performance Gaming Mouse</a>
+                                        </li>
+                                        <li>
+                                            <a href="../products/Razer_DeathAdder_Essential_Gaming_Mouse.php">Razer DeathAdder Essential Gaming Mouse</a>
+                                        </li>
+										<li>
+                                            <a href="../products/Steelseries_Aerox_3_Wireless_Lightweight_Gaming_Mouse.php">Steelseries Aerox 3 Wireless Lightweight Gaming Mouse</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">PC Office</a>
+                            <div class="toggle"></div>
+                            <ul>
+                                <li>
+                                    <a href="#">Office Mice</a>
+                                    <div class="toggle"></div>
+                                    <ul>
+                                        <li>
+                                            <a href="../products/Logitech_M170_Wireless_Mouse.php">Logitech M170 Wireless Mouse</a>
+                                        </li>
+                                        <li>
+                                            <a href="../products/Logitech_MX_Master_3_Wireless_Mouse.php">Logitech MX Master 3 Wireless Mouse</a>
+                                        </li>
+                                        <li>
+                                            <a href="../products/Logitech_M325_Wireless_Mouse.php">Logitech M325 Wireless Mouse</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">Office Keyboard</a>
+                                    <div class="toggle"></div>
+                                    <ul>
+                                        <li>
+                                            <a href="../products/Logitech_K380_Slim_Multi-Device_Keyboard.php">Logitech K380 Slim Multi-Device Keyboard</a>
+                                        </li>
+                                        <li>
+                                            <a href="../products/Microsoft_Bluetooth_Desktop_Combo_Keyboard.php">Microsoft Bluetooth Desktop Combo Keyboard</a>
+                                        </li>
+										<li>
+                                            <a href="../products/Targus_KB55_Multi-Platform_Bluetooth.php">Targus KB55 Multi-Platform Bluetooth</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">Webcam</a>
+                                    <div class="toggle"></div>
+                                    <ul>
+                                        <li>
+                                            <a href="../products/Logitech_C922_Pro_HD_Stream_Webcam.php">Logitech C922 Pro HD Stream Webcam</a>
+                                        </li>
+                                        <li>
+                                            <a href="../products/J5Create_USB_HD_Webcam_with_360°_Rotation_(JVCU100).php">J5Create USB HD Webcam with 360° Rotation (JVCU100)</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Laptop</a>
+                            <div class="toggle"></div>
+                            <ul>
+                                <li>
+                                    <a href="#">Consumer Laptop</a>
+                                    <div class="toggle"></div>
+                                    <ul>
+                                        <li>
+                                            <a href="../products/MSI_Modern_15_15.6″_FHD_Laptop.php">MSI Modern 15 15.6″ FHD Laptop</a>
+                                        </li>
+                                        <li>
+                                            <a href="../products/Acer_Swift_3x_SF314-510G-761J.php">Acer Swift 3x SF314-510G-761J 14” FHD Laptop Steam Blue</a>
+                                        </li>
+                                        <li>
+                                            <a href="../products/Asus_ZenBook_13 UX325E-AKG349TS 13.3”_OLED_FHD_Laptop_Pine_Grey.php">Asus ZenBook 13 UX325E-AKG349TS 13.3” OLED FHD Laptop Pine Grey</a>
+                                        </li>
+                                    </ul>
+                                </li>
+								<li>
+                                    <a href="#">Gaming Laptop</a>
+                                    <div class="toggle"></div>
+                                    <ul>
+                                        <li>
+                                            <a href="../products/MSI_GF63_Thin_15.6″_FHD_Gaming_Laptop.php">MSI GF63 Thin 15.6″ FHD Gaming Laptop </a>
+                                        </li>
+                                        <li>
+                                            <a href="../products/Asus_TUF_Dash_F15_FX516P-MHN085T_15.6″FHD.php">Asus TUF Dash F15 FX516P-MHN085T 15.6″FHD</a>
+                                        </li>
+                                        <li>
+                                            <a href="../products/Acer_Nitro_5_AN515-45-R7QR_15.6″_Gaming_Laptop_Black.php">Acer Nitro 5 AN515-45-R7QR 15.6″ Gaming Laptop Black</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                             
+                    </ul>
+
+                    <div class="empty-space col-xs-b25 col-sm-b50"></div>
+           
+                </div> 
+            </div>
+            </div>
+        </div>
+
+<?php
+include_once '../include/footer.php';
+?>
+<?php
+include_once '../include/header_popup.php';
+?>
