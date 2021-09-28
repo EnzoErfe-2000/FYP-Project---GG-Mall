1674bd8ef03dda0263a23b8f2966968fb807e73c
1191202274 <behjiamin9@gmail.com>
2021-09-28 18:49:17 +0800

    -done for contact page and change some file to php

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@ include_once 'include/header.php';
 
         <div class="header-empty-space"></div>
 
-        <div class="block-entry fixed-background" style="background-image: url(img/background-26.jpg);">
+        <div class="block-entry fixed-background" style="background-image: url(/fyp-project/img/background-2.jpg);">
             <div class="container">
                 <div class="row">
                     <div class="col-sm-6 col-sm-offset-3">
@@ -12,7 +12,8 @@ include_once 'include/header.php';
                             <div class="empty-space col-xs-b35 col-sm-b70"></div>
                             <h1 class="h1 light">contact us</h1>
                             <div class="title-underline center"><span></span></div>
-                            <div class="simple-article light transparent size-4">We are *************</div>
+                            <div class="simple-article light transparent size-4">We are D720's 1st and Largest IT Hardware Store. We strive towards our longtime motto "fulfilling all your IT needs" and to serve you better.
+</div>
                             <div class="empty-space col-xs-b35 col-sm-b70"></div>
                         </div>
                     </div>
@@ -38,8 +39,9 @@ include_once 'include/header.php';
                 <div class="col-sm-3">
                     <div class="icon-description-shortcode style-1">
                         <img class="icon" src="img/icon-25.png" alt="">
+						
                         <div class="title h6">address</div>
-                        <div class="description simple-article size-2">1st, new york, usa</div>
+                        <div class="description simple-article size-2">JALAN AYER KEROH LAMA, 75450 BUKIT BERUANG, MELAKA</div>
                     </div>
                 </div>
                 <div class="col-sm-3">
@@ -47,9 +49,7 @@ include_once 'include/header.php';
                         <img class="icon" src="img/icon-23.png" alt="">
                         <div class="title h6">phone</div>
                         <div class="description simple-article size-2" style="line-height: 26px;">
-                            <a href="tel:+35235551238745">+3  (523) 555 123 8745</a>
-                            <br/>
-                            <a href="tel:+35235557585238">+3  (523) 555 758 5238</a>
+                            <a href="tel:+35235551238745">+0121314520</a>
                         </div>
                     </div>
                 </div>
@@ -57,7 +57,7 @@ include_once 'include/header.php';
                     <div class="icon-description-shortcode style-1">
                         <img class="icon" src="img/icon-28.png" alt="">
                         <div class="title h6">email</div>
-                        <div class="description simple-article size-2"><a href="mailto:offce@exzo.com">offce@exzo.com</a></div>
+                        <div class="description simple-article size-2"><a href="mailto:ggmall_inc@gmail.com">ggmall_inc@gmail.com</a></div>
                     </div>
                 </div>
                 <div class="col-sm-3">
@@ -77,14 +77,17 @@ include_once 'include/header.php';
 
         <div class="empty-space col-xs-b25 col-sm-b50"></div>
     
-        <div class="container">    
-            <div class="map-wrapper">
-                <div id="map-canvas" class="full-width" data-lat="34.0151244" data-lng="-118.4729871" data-zoom="14"></div>
-            </div>
-            <div class="addresses-block hidden">
-                <a class="marker" data-lat="34.0151244" data-lng="-118.4729871" data-string="1. Here is some address or email or phone or something else..."></a>
-            </div>
-        </div>
+        <div class="container">
+			<div class="gmap_canvas">
+				<iframe width="1120" height="404" id="gmap_canvas" src="https://maps.google.com/maps?q=ALAN%20AYER%20KEROH%20LAMA,%2075450%20BUKIT%20BERUANG,%20MELAKA&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+				<a href="https://fmovies-online.net"></a>
+				<br>
+				<style>{position:center;text-align:center;height:404px;width:1200px;}</style>
+				<a href="https://www.embedgooglemap.net">google maps websites</a>
+				
+				<style>.gmap_canvas {overflow:hidden;background:none!important;height:404px;width:1200px;}</style>
+			</div>
+		</div>
 
         <div class="empty-space col-xs-b25 col-sm-b50"></div>
 
