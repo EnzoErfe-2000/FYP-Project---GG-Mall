a2decac4f259491b9f4db395e7507bd382e2c9a7
1191202274 <behjiamin9@gmail.com>
2021-10-02 21:54:00 +0800

    clear branch

--- db/common.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $con = mysqli_connect("localhost", "root", "", "gg_mall") or die(mysqli_error($con));
-    if(!isset($_SESSION)){
-      session_start();
-    }
-?>
-
--- include/logout-inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-//header("location: ../index.php");
-$uri = $_SERVER['HTTP_REFERER'];
-//header("location: ".$uri);
-header("location: /fyp-project/index.php");
-?>
