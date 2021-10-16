9ff8ada84065abe494f8017bb68191b28f3aaa5a
Adelene Ee <eepeiying@gmail.com>
2021-10-17 00:39:36 +0800

    test

--- admin/admin-login.php
+++ admin/admin-login.php
@@ -1,29 +1,33 @@
 <?php
-require('../db/common.php');
+require('../include/dbh-inc.php');
 
 
 if (isset($_POST['username'])){
         // removes backslashes
 	$username = stripslashes($_REQUEST['username']);
         //escapes special characters in a string
-	$username = mysqli_real_escape_string($con,$username);
+	$username = mysqli_real_escape_string($conn,$username);
 	$password = stripslashes($_REQUEST['password']);
-	$password = mysqli_real_escape_string($con,$password);
+	$password = mysqli_real_escape_string($conn,$password);
 	//Checking is user existing in the database or not
-        $query = "SELECT * FROM `users` WHERE username='$username'
-and password='".md5($password)."'";
-	$result = mysqli_query($con,$query) or die(mysql_error());
+    $query = "SELECT * FROM `users` WHERE username='$username'and password='".md5($password)."'";
+	$result = mysqli_query($conn,$query) or die(mysql_error());
 	$rows = mysqli_num_rows($result);
-        if($rows==1){
-	    $_SESSION['username'] = $username;
-            // Redirect user to index.php
-	    header("Location: index.php");
-         }else{
-	echo "<div class='form'>
-<h3>Username/password is incorrect.</h3>
-<br/>Click here to <a href='login.php'>Login</a></div>";
+	if($rows==1)
+	{
+		$_SESSION['username'] = $username;
+			// Redirect user to index.php
+		header("Location: index.php");
 	}
-    }else{
+	else
+	{
+		echo "<div class='form'>
+		<h3>Username/password is incorrect.</h3>
+		<br/>Click here to <a href='login.php'>Login</a></div>";
+	}
+}
+else
+{
 ?>
 <div class="form">
 <h1>Log In</h1>
