d0181be134c3c4e6b1402b519798fe62864e3327
1191202274 <behjiamin9@gmail.com>
2021-10-16 12:06:18 +0800

    modified productlist

--- admin/include/adminfooter.php
+++ admin/include/adminfooter.php
@@ -23,7 +23,20 @@
   <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
   <script src="js/ruang-admin.min.js"></script>
   <script src="vendor/chart.js/Chart.min.js"></script>
-  <script src="js/demo/chart-area-demo.js"></script>  
+  <script src="js/demo/chart-area-demo.js"></script>
+  <script src="js/demo/chart-bar-demo.js"></script> 
+<script src="js/demo/chart-pie-demo.js"></script>  
+  <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script>
+    $(document).ready(function () {
+      $('#dataTable').DataTable(); // ID From dataTable 
+      $('#dataTableHover').DataTable(); // ID From dataTable with Hover
+    });
+  </script>
+
 </body>
 
 </html>
